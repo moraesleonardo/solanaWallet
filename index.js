114a795649f60d194ed432d9fef4d2d2bebb5878
Leonardo <leonardobastosmoraes@hotmail.com>
2024-07-29 07:05:20 -0300

    Checking balance after airdrop for new wallet

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const secretKey = wallet._keypair.secretKey
 
 const getWalletBalance = async() => {
     try {
-        const connection = new Connection(clusterApiUrl('devnet', 'confirmed'))
+        const connection = new Connection('http://127.0.0.1:8899', 'confirmed')
+        //const connection = new Connection(clusterApiUrl('testnet', 'confirmed'))
+        //const connection = new Connection(clusterApiUrl('devnet', 'confirmed'))
         const walletBalance = await connection.getBalance(publicKey)
         console.log(`Wallet balance is ${walletBalance}`)
         
@@ -23,8 +25,29 @@ const getWalletBalance = async() => {
     }
 }
 
+const airDropSol = async() => {
+    try {
+        const connection = new Connection('http://127.0.0.1:8899', 'confirmed')
+        //const connection = new Connection(clusterApiUrl('testnet', 'confirmed'))
+        //const connection = new Connection(clusterApiUrl('devnet', 'confirmed'))
+        const walletBalance = await connection.getBalance(publicKey)
+        const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2* LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(fromAirDropSignature)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const main = async() => {
-    await getWalletBalance()
+    
+    //console.log('Starting balance check for new wallet...');
+    //await getWalletBalance();
+    //console.log('Starting airdrop for new wallet...');
+    //await airDropSol();
+    console.log('Checking balance after airdrop for new wallet...');
+    await getWalletBalance();
+    
 }
 
 main()
